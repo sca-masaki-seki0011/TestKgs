18df2a138d1a4d64105afc278401167db9911315
makki <21cge0011sm@edu.sca.ac.jp>
2023-12-05 19:53:22 +0900

    ロード途中

--- Assets/Script/Ogawa/TitleScene/SousaUIContorller.cs
+++ Assets/Script/Ogawa/TitleScene/SousaUIContorller.cs
@@ -43,8 +43,9 @@ public class SousaUIContorller : MonoBehaviour
     
     //‚±‚±‰ü—Ç‚·‚é
     IEnumerator LoadScene() {
+        yield return null;
         AsyncOperation async = SceneManager.LoadSceneAsync(TitleManager.sceneName);
-        //async.allowSceneActivation = false;
+        async.allowSceneActivation = false;
         while(!async.isDone) {
             _slider.value = async.progress;
 
@@ -53,9 +54,9 @@ public class SousaUIContorller : MonoBehaviour
             //Pagecount = (int)async.progress%5;
             if(async.progress >= 0.9f) {
                 _text.text = "100%";
-                //if(Gamepad.current.bButton.isPressed) {
-                    //async.allowSceneActivation = true;
-                //}
+                if(Gamepad.current.bButton.isPressed) {
+                    async.allowSceneActivation = true;
+                }
             }
             yield return null;
         }
