0e407ec7ad42d01c3420a08e5481d3bfb98cbd05
makki <21cge0011sm@edu.sca.ac.jp>
2023-12-14 10:50:36 +0900

    ロード画面は一旦完了で後で確認させる次は猫とお宝のミッションをやる

--- Assets/Script/Ogawa/TitleScene/SousaUIContorller.cs
+++ Assets/Script/Ogawa/TitleScene/SousaUIContorller.cs
@@ -35,12 +35,15 @@ public class SousaUIContorller : MonoBehaviour
     int randomtips = 0;
     AsyncOperation async;
 
-    [SerializeField] Animator anim;
-   
+    [SerializeField] GameObject playerObj;
+    Vector3 pos;
+    Animator playeranim;
     // Start is called before the first frame update
 
     void Start()
     {
+        pos = playerObj.transform.position;
+        playeranim = playerObj.GetComponent<Animator>();
         okText.SetActive(false);
         
         _slider = sliderBar.GetComponent<Slider>();
@@ -58,7 +61,7 @@ public class SousaUIContorller : MonoBehaviour
 
     public void LoadNextScene() {
         _loadingUI.SetActive(true);
-        StartCoroutine(LoadScene());
+        //StartCoroutine(LoadScene());
     }
     
     bool ok = false;
@@ -69,12 +72,13 @@ public class SousaUIContorller : MonoBehaviour
         async = SceneManager.LoadSceneAsync(TitleManager.sceneName);
         async.allowSceneActivation = false;
         while(!async.isDone) {
-            //_slider.value = async.progress;
+            //_slider.value = async.progress*100;
 
             _text.text = (async.progress * 100).ToString() + "%";
 
             //Pagecount = (int)async.progress%5;
             if(async.progress >= 0.9f) {
+                _slider.value = 100.0f;
                 _text.text = "100%";
                 
                 StartCoroutine(Waitok());
@@ -91,29 +95,41 @@ public class SousaUIContorller : MonoBehaviour
         //sliderBar.SetActive(false);
         _text.enabled = false;
         okText.SetActive(true);
+        playerObj.SetActive(false);
         ok = true;
     }
 
+    bool move = false;
 
     // Update is called once per frame
     void Update()
     {
         
-        Debug.Log(Pagecount);
+        Debug.Log(_slider.value);
+    
+        if(_slider.value != 0 && _slider.value % 10 == 0 && _slider.value < 100.0f) {
+           move = true; 
+        }
+
+        if(move) {
+            pos.x += 1.8f;
+            playerObj.transform.position = pos;
+            move = false;
+        }
    
-        if(!tips) {
+        if(!tips && _slider.value < 100.0f) {
             tipsTime += Time.deltaTime;
             if((int)tipsTime != 0 && (int)tipsTime % 5 == 0) {
                 tips = true;
             }
         }
        
-        if(_slider.value >= 1.0f) {
-            anim.SetBool("Fall",true);
+        if(_slider.value >= 100.0f) {
+            playeranim.SetBool("Fall",true);
         }
 
         if(tips) {
-            _slider.value += 0.1f;
+            //_slider.value += 10f;//デバックようにスライダーの値をいじっただけ
             tips = false;
             if(Pagecount == 4) {
                 Pagecount = 0;
