7131f315ea8b13bb9a53ecde175e1db01e311907
makki <21cge0011sm@edu.sca.ac.jp>
2023-12-17 19:15:00 +0900

    ネコの移動を後でらーぷでやってみる

--- Assets/Seki/Scripts/CatController.cs
+++ Assets/Seki/Scripts/CatController.cs
@@ -7,7 +7,7 @@ using UnityEngine.UIElements;
 public class CatController : MonoBehaviour
 {
     [SerializeField] private GameObject[] m_gameObject;
-    float speed = 1.0f;
+    float speed = 5.0f;
     int dirastionCount = -1;
     public int DIRATIONCOUNT
     {
@@ -26,47 +26,73 @@ public class CatController : MonoBehaviour
         this.transform.position = m_gameObject[0].transform.position;
     }
 
+    int dire = 0;
+    bool stop = false;
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(dirastionCount);
-        if(dirastionCount != -1) { 
-        if(this.transform.position == m_gameObject[0].transform.position) {
-             Debug.Log("拠点");
-             if(dirastionCount == 0 || dirastionCount == 1) {
+        Debug.Log("カウント"+ dirastionCount+ "方向"+dire);
+        if(dirastionCount != -1 && !stop) {
+            CatMoveCount(CatPos());
+        }
 
-                CatMovbe(1);
-             }
-             
-             if(dirastionCount == 2) {
-                CatMovbe(3);
-            }
+
+        if(this.transform.position == m_gameObject[3].transform.position) {
+            Debug.Log("拠点1");
+            
         }
+    }
+
+    void CatMoveCount(int c) {
+        
+        switch(c) {
+            case 1:
+                if(dirastionCount == 0 || dirastionCount == 1) {
+                    CatMovbe(3);
+                }
+                if(dirastionCount == 2) {
+                    CatMovbe(1);
+                }
+                break;
+            case 2:
+                if(dirastionCount == 0 || dirastionCount == 1) {
+                    CatMovbe(0);
+                }
+                if(dirastionCount == 2) {
+                    CatMovbe(2);
+                }
+                break;
+            case 3:
+                if(dirastionCount == 0 || dirastionCount == 1) {
+                    CatMovbe(1);
+                }
+                break;
+            case 4:
+                if(dirastionCount == 0 || dirastionCount == 1) {
+                    CatMovbe(0);
+                }
+                break;
         }
-        /*
+    }
+
+    int CatPos() {
+        if(this.transform.position == m_gameObject[0].transform.position) {
+            dire = 1;
+        }
         if(this.transform.position == m_gameObject[1].transform.position) {
-            if(dirastionCount == 0 || dirastionCount == 1) {
-                CatMovbe(0);
-            }
-            if(dirastionCount == 2) {
-                CatMovbe(2);
-            }
+        
+            dire = 2;
         }
         if(this.transform.position == m_gameObject[2].transform.position) {
-            if(dirastionCount == 0 || dirastionCount == 1) {
-                CatMovbe(1);
-            }
-         
+            dire = 3;
         }
         if(this.transform.position == m_gameObject[3].transform.position) {
-            if(dirastionCount == 0 || dirastionCount == 1) {
-                //CatMovbe(0);
-            }
-        
+            dire = 4;
         }
-        //CatMovbe(dirastionCount);
-        */
+        return dire;
     }
+
+    
 
     void CatMovbe(int c) {
         // 移動量を計算
@@ -81,14 +107,20 @@ public class CatController : MonoBehaviour
 
         // オブジェクトの回転に反映
         this.transform.rotation = rotation;
+        /*ここの動作を後でらーぷでやってみる
         float step = speed * Time.deltaTime;
         this.transform.position = Vector3.MoveTowards(this.transform.position, m_gameObject[c].transform.position, step);
+        */
         StartCoroutine(WaitFlag());
-        
+    
     }
 
     IEnumerator WaitFlag() {
-        yield return new WaitForSeconds(3.0f);
+        
+        yield return new WaitForSeconds(1.01f);
+        stop = true;
+        dire = 0;
+        dirastionCount = -1;
         player.enabled = true;
     }
 }
