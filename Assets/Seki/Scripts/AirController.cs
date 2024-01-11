d9a1b0b94c46cdafbb4a42e0688af0e15a3c9aa9
makki <21cge0011sm@edu.sca.ac.jp>
2024-01-12 00:38:18 +0900

    飛行機移動はできたからあとはビルにぶつからせてそれを阻止する流れを作るのと各ミッションのUIを画面外になった場合のUIの動きとシナリオを作る

---
+++ Assets/Seki/Scripts/AirController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirController : MonoBehaviour
+{
+    [SerializeField] GameObject Pos;
+    float speed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var delta = this.transform.position - Pos.transform.position;
+
+        // 静止している状態だと、進行方向を特定できないため回転しない
+        if(delta == Vector3.zero)
+            return;
+
+        // 進行方向（移動量ベクトル）に向くようなクォータニオンを取得
+        //var rotation = Quaternion.LookRotation(delta, Vector3.up);
+
+        // オブジェクトの回転に反映
+        //this.transform.rotation = rotation;
+
+
+        float step = speed * Time.deltaTime;
+        this.transform.position = Vector3.MoveTowards(this.transform.position, Pos.transform.position, step);
+    }
+}
