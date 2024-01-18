b3e68f270623f09381383933d8524cd88126fdec
makki <21cge0011sm@edu.sca.ac.jp>
2024-01-18 14:08:47 +0900

    キューブにてスロープできたかも

--- Assets/Seki/Scripts/KaidanC.cs
+++ Assets/Seki/Scripts/KaidanC.cs
@@ -8,7 +8,9 @@ public class KaidanC : MonoBehaviour
     
     [SerializeField] Transform[] Pos;
     int destPoint = 1;
-    float speed = 20.0f;
+    float speed = 4.0f;
+    bool slope = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,17 +20,22 @@ public class KaidanC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S)) { 
-        //if(!agent.pathPending && agent.remainingDistance < 0.5f) {
-            if(destPoint >= Pos.Length-1) {
+        if(slope) { 
+        if(this.transform.position == Pos[destPoint].transform.position) {
+            //Debug.Log("到着");
+            
+            if(destPoint >= Pos.Length - 1) {
                 destPoint = 0;
+                slope = false;
             } else {
                 destPoint++;
             }
             
         }
+        
         GotoNextPoint(destPoint);
-        Debug.Log("ポイント"+destPoint);
+        Debug.Log("ポイント"+ destPoint);
+        }
     }
 
     void GotoNextPoint(int c) {
@@ -47,6 +54,7 @@ public class KaidanC : MonoBehaviour
 
         float step = speed * Time.deltaTime;
         this.transform.position = Vector3.MoveTowards(this.transform.position, Pos[c].transform.position, step);
+        
         //if(Pos.Length == 0) {
         //return;
         //}
